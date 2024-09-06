8464bc37d6595a6420e1c287bdb4b42d979014af
Khanyie1 <khanyisanimajozi98@gmail.com>
2024-09-06 23:52:38 +0100

    The integrated UniApplyBot

---
+++ public/UniApplyBot/script.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('.navbar a, .mobile-menu a');
+
+    const observerOptions = {
+      root: null, 
+      rootMargin: '0px',
+      threshold: 0.2 
+    };
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+          console.log(entry.target.id);
+        if (entry.isIntersecting) {
+          const sectionID = entry.target.getAttribute('id');
+          navLinks.forEach(link => {
+            link.classList.remove('active');
+            if (link.getAttribute('href') === '#' + sectionID) {
+              link.classList.add('active');
+            }
+          });
+        }
+      });
+    }, observerOptions);
+
+    sections.forEach(section => {
+      observer.observe(section);
+    });
+
+    document.getElementById('toggleBotButton').addEventListener('click', function() {
+      const botElement = document.querySelector('df-messenger');
+      if (botElement.style.display === 'none') {
+          botElement.style.display = 'block';
+          this.textContent = 'Bot';
+      } else {
+          botElement.style.display = 'none';
+          this.textContent = 'Bot';
+      }
+    });
+
+    const menuIcon = document.getElementById('menu-icon');
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    menuIcon.addEventListener('click', function() {
+
+        mobileMenu.classList.toggle('show');
+    });
+});
---
+++ public/frontend/login.js
@@ -0,0 +1,37 @@
+// Toggling the visibility of the password
+const eye = document.getElementById('eye');
+let state = false;
+
+function toggle() {
+    const passwordField = document.getElementById('password');
+    
+    if (state) {
+        passwordField.setAttribute('type', 'password');
+        eye.style.color = '#7a797e';
+        state = false;
+    } else {
+        passwordField.setAttribute('type', 'text');
+        eye.style.color = '#d23e3e';
+        state = true;
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Handling the login function
+    const login = document.getElementById('login');
+
+    function loginFunc() {
+        login.addEventListener('click', (e) => {
+            e.preventDefault();
+            alert("Now on Home Page...");
+
+            console.log("Navigating to: ../UniApplyBot/index.html");
+
+            location.href = "../UniApplyBot/index.html";
+        });
+    }
+    loginFunc();
+});
+
+
