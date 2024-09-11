53caa9e7d7d7b0f820c5f29a8976c0268ed0f615
Khanyie1 <khanyisanimajozi98@gmail.com>
2024-09-11 11:01:27 +0100

    latest front end changes

--- public/frontend/login.js
+++ public/frontend/login.js
@@ -1,19 +1,3 @@
-// Toggling the visibility of the password
-const eye = document.getElementById('eye');
-let state = false;
-
-function toggle() {
-    const passwordField = document.getElementById('password');
-    
-    if (state) {
-        passwordField.setAttribute('type', 'password');
-        eye.style.color = '#7a797e';
-        state = false;
-    } else {
-        passwordField.setAttribute('type', 'text');
-        eye.style.color = '#FFFFFF';
-        state = true;
-
 document.addEventListener('DOMContentLoaded', () => {
     // Toggling the visibility of the password
     const eyeBtn = document.getElementById('eye');
