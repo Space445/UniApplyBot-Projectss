e59593d210a8295de84aedce519fcf9c326f4913
OLebse@95 <fentselebese@gmail.com>
2024-09-18 11:02:01 +0200

    More changes, styling and about us

--- public/UniApplyBot/script.js
+++ public/UniApplyBot/script.js
@@ -84,3 +84,40 @@ try {
 } catch {
   console.log("Failed to log out.")
 }
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// FAQ Collapser
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active1");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
